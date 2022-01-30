1669b52c4abe0fee1c7274c8917eb56ade533869
GitHub Action <action@github.com>
2022-01-30 13:29:56 +0000

    Template cleanup

--- src/main/kotlin/com/github/suck1n/keystrokesound/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/suck1n/keystrokesound/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.suck1n.keystrokesound.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.suck1n.keystrokesound.MyBundle
 
 class MyApplicationService {
 
